9164664b7ec67af207c656fd9a0f6e1b5c57c350
Przemo12121 <49036565+Przemo12121@users.noreply.github.com>
2023-05-02 16:46:57 +0200

    added dynamics

--- zad3/src/App.tsx
+++ zad3/src/App.tsx
@@ -3,7 +3,32 @@ import "./App.css";
 import data from "./data.json";
 
 function App() {
+  const [citySearch, setCitySearch] = useState("");
+  const [roomsSearch, setRoomsSearch] = useState("");
+  const [descriptionSearch, setDescriptionSearch] = useState("");
   const [ads, setAds] = useState(data);
+
+  const filter = () => {
+    let filtered = data;
+
+    if (citySearch) {
+      filtered = data.filter(
+        ad => ad.address.city.toLowerCase().includes(citySearch.toLowerCase())
+      );
+    }
+    if (descriptionSearch) {
+      filtered = data.filter(
+        ad => ad.description.toLowerCase().includes(descriptionSearch.toLowerCase())
+      );
+    }
+    if (roomsSearch) {
+      filtered = data.filter(
+        ad => ad.sleepingRooms === Number.parseInt(roomsSearch)
+      );
+    }
+
+    setAds(filtered);
+  }
 
   const adHolders = useMemo(
     () => ads.map(ad => 
@@ -11,7 +36,9 @@ function App() {
         <div className="ad-text">
           <p>{ad.address.city}, {ad.address.street}</p>
           <p>{ad.description}</p>
-          <p className="ad-footer">price: <b>{ad.pricePerMonth}</b>, owner: <b>{ad.owner}, {ad.residentialArea}m2, {ad.sleepingRooms} sleeping rooms</b></p>
+          <p className="ad-footer">
+            price: <b>{ad.pricePerMonth}</b>, owner: <b>{ad.owner}</b>, params: {ad.residentialArea}m2, {ad.sleepingRooms} sleeping rooms
+          </p>
         </div>
 
         <img src={ad.image} alt={ad.image}/>
@@ -22,15 +49,29 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Rent-a-house</h1>
-      <h2>Przemysław Małecki</h2>
+      <div>
+        <h1>Rent-a-house</h1>
+        <h2>by: Przemysław Małecki</h2>
+      </div>
       
       <div className="input-container">
-        <input placeholder="search" />
-        <input placeholder="search" />
-        <input placeholder="search" />
+        <span>
+          City: <input placeholder="search" onChange={(e) => setCitySearch(e.target.value)} />
+        </span>
+        <span>
+          Sleep rooms: <input placeholder="search" onChange={(e) => setRoomsSearch(e.target.value)} />
+        </span>
+        <span>
+          Description: <input placeholder="search" onChange={(e) => setDescriptionSearch(e.target.value)} />
+        </span>
 
-        <button>Search</button>
+        <button onClick={filter}>Search</button>
+      </div>
+
+      <div className="input-container">
+        Order by price:
+        <button>ascending</button>
+        <button>descending</button>
       </div>
       
       <div className="ad-container">
